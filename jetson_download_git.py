11bf27e065b044f0972852044c7240359d3aee39
crazyboy9103 <gil9103@gmail.com>
2021-09-11 10:02:25 +0000

    final commit before distribution

--- jetson_download_git.py
+++ jetson_download_git.py
@@ -1,4 +1,5 @@
 import paramiko
+import sys
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--command", type=str, default="ls", help="linux command to execute globally")
@@ -20,72 +21,91 @@ COMMAND = args.command
 available = []
 
 
-
-def ping():
-    for port in range(int(MIN_PORT), int(MAX_PORT)+1):
-        if port % 10 > 6 or port % 10 == 0:
-            continue
-
-        cli = paramiko.SSHClient()
-        #cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        cli.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
-        try:
-            cli.connect(JETSON_IP, port=port, username=USERNAME, password=PASSWORD)
-
-            stdin, stdout, stderr = cli.exec_command("ls")
-            lines = stdout.readlines()
-            
-            if lines:
-                available.append(port)
-                print(f"{port} is working: ls {lines}")
-
-            cli.close()
-
-        except Exception as e:
-            print(f"{port} {e}")
-            continue
-    
-    temp = [i for i in range(int(MIN_PORT), int(MAX_PORT)+1)]
-    result = []
-
-    for port in temp:
-        if port % 10 <= 6 and port % 10 > 0:
-            result.append(port)
-
-    print("NOT CONNECTED PORTS : ", list(set(result)-set(available)))
-
-
+class GlobalExecute:
+    def __init__(self):
+        self.cli = paramiko.SSHClient()
+        self.cli.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
         
+        self.clients = {}
+        self.available = []
 
-def global_exec(command):
-    # command : linux command to execute 
-    print(f"Executes '{command}'")
-    for port in available:
-        try:
-            cli = paramiko.SSHClient()
-            cli.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
-            #cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        pass
 
-            cli.connect(JETSON_IP, port=port, username=USERNAME, password=PASSWORD)
+    def ping(self):
+        self.available = []
+        for port in range(int(MIN_PORT), int(MAX_PORT)+1):
+            if port % 10 > 6 or port % 10 == 0:
+                continue
+
+            try:
+                self.available.append(port)
+
+                cli = paramiko.SSHClient()
+                cli.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
+                cli.connect(JETSON_IP, port=port, username=USERNAME, password=PASSWORD)
+
+                stdin, stdout, stderr = cli.exec_command("ls")
+                lines = stdout.readlines()
             
-            _, stdout, stderr = cli.exec_command(f"{command}")
+                if lines:
+                    self.clients[port] = cli
+                    print(f"{port} is working: ls {lines}")
+
+                
+            except Exception as e:
+                print(f"{port} {e}")
+                continue
+    
+        temp = [i for i in range(int(MIN_PORT), int(MAX_PORT)+1)]
+        result = []
+
+        for port in temp:
+            if port % 10 <= 6 and port % 10 > 0:
+                result.append(port)
+
+        print("NOT CONNECTED PORTS : ", list(set(result)-set(self.available)))
+
+    def global_exec(self, command):
+        # command : linux command to execute 
+        print(f"Executes '{command}'")
+        for port in self.available:
+            try:
+                cli = self.clients[port]
+                stdin, stdout, stderr = cli.exec_command(f"{command}")
             
-            if stdout:
-                stdout = stdout.readlines()
-            if stderr:
-                stderr = stderr.readlines()
+                if stdout:
+                    stdout = stdout.readlines()
+                if stderr:
+                    stderr = stderr.readlines()
             
-            if stdout:
                 print(f"Port {port} OUTPUT {stdout}")
-            if stderr:
-                print(f"Port {port} ERROR {stderr}")
+                
+                if stderr:
+                    print(f"Port {port} ERROR {stderr}")
             
 
-        except Exception as e:
-            print(f"Port {port} error {i}")
-            continue
+            except Exception as e:
+                print(f"Port {port} error {e}")
+                continue
 
 
+    
 if __name__ == "__main__":
-    ping()
-    global_exec(COMMAND)
+    cl = GlobalExecute()
+    cl.ping()
+    cl.global_exec(COMMAND)
+    
+    res = "y"
+    while res == "y":
+        res = input("More commands? (y/n)\n")
+        if res == "y":
+            new_command = input("new command:\n")
+            new_command.strip("\n")
+            cl.global_exec(str(new_command))
+        elif res == "n":
+            for k, v in cl.clients.items():
+                v.close()
+            sys.exit()
+
+
+            
