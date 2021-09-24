9bc33345b0b43b7bd52b2019a70cefc1c9634267
crazyboy9103 <gil9103@gmail.com>
2021-09-24 05:56:19 +0000

    fix

--- FL_Client/jetson_client.py
+++ FL_Client/jetson_client.py
@@ -319,12 +319,12 @@ class Client:
         """
         local_weight_to_json = json.dumps(local_weight, cls=NumpyEncoder)
         local_weight = {"fed_id":self.fed_id, "weights": local_weight_to_json}
-        local_weight = json.dumps(local_weight) 
+        #local_weight = json.dumps(local_weight) 
         requests.put(self.weight_url, data=local_weight)
         
     def upload_local_accuracy(self, accuracy):
         #first index of accuracy is accuracy (0 is loss) 
-        temp_dict = {'accuracy':accuracy[1], 'fed_id':self.fed_id}
+        temp_dict = {'accuracy':accuracy, 'fed_id':self.fed_id}
         local_acc_to_json = json.dumps(temp_dict)
         requests.put(self.accuracy_url, data=local_acc_to_json)
         
