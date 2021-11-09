973221c529068a5d2d4885aa931b6ad81f816123
crazyboy9103 <gil9103@gmail.com>
2021-11-09 16:15:10 +0900

    django in conda

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-cd /home/ambient_fl/FL_Server && pip3 install -q -r requirements.txt && python3 manage.py runserver 0.0.0.0:9103
+cd ./FL_Server && pip3 install -r requirements.txt && python3 manage.py runserver 0.0.0.0:9103
