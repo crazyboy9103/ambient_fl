a4f2ea5eb67c85049833c732a53ad32c98e9cdd6
crazyboy9103 <gil9103@gmail.com>
2021-11-09 14:22:15 +0900

    error fixed

--- FL_Server/app/views.py
+++ FL_Server/app/views.py
@@ -9,7 +9,6 @@ from app.Federated import FederatedServer
 
 @api_view(['GET'])
 def index(request):
-    logger.info("request index")
     return HttpResponse("index ok", status.HTTP_200_OK)
 
 @api_view(['GET'])
@@ -29,18 +28,18 @@ def set_experiment(request, experiment):
         FederatedServer.experiment = experiment
     else:
         FederatedServer.experiment = 1
-    return HttpResponse("Request PUT OK", status.HTTP_200_OK)
+    return HttpResponse(f"Experiment set to {FederatedServer.experiment}", status.HTTP_200_OK)
 
 @api_view(['PUT'])
 def put_accuracy(request, client_id):
     json_data = JSONParser().parse(request)
     FederatedServer.accuracy[client_id] = float(json_data['accuracy'])
-    return HttpResponse("Request PUT OK", status.HTTP_200_OK)
+    return HttpResponse(f"Accuracy for client {client_id} is updated", status.HTTP_200_OK)
 
 @api_view(['PUT'])
 def set_client_num(request, client_num):
     FederatedServer.client_number = client_num
-    return HttpResponse("Request PUT OK", status.HTTP_200_OK)
+    return HttpResponse(f"Total client number is set to {client_num}", status.HTTP_200_OK)
 
 @api_view(['GET'])
 def get_server_weight(request):
@@ -53,7 +52,7 @@ def put_weight(request, client_id):
     json_data = JSONParser().parse(request)
     weights = list(json_data['weights'])
     FederatedServer.update(client_id, weights)
-    return HttpResponse("Request PUT OK", status.HTTP_200_OK)
+    return HttpResponse(f"Weight of client {client_id} is updated", status.HTTP_200_OK)
 
 @api_view(['PUT'])
 def update_num_data(request, client_id, num_data):
@@ -70,6 +69,5 @@ def reset(request):
 
 @api_view(['GET'])
 def initialize(request, client_num, experiment, max_round):
-   
     return HttpResponse(FederatedServer.initialize(client_num, experiment, max_round), status.HTTP_200_OK)
 
---
+++ run_server.sh
@@ -0,0 +1,1 @@
+cd /home/ambient_fl/FL_Server && pip3 install -q -r requirements.txt && python3 manage.py runserver 0.0.0.0:9103
