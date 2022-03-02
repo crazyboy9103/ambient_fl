cd3557e3a4e43526ad745c93a2a9ce8173eae048
crazyboy9103 <gil9103@gmail.com>
2022-03-02 14:23:42 +0900

    client_jetson.py

--- client_jetson.py
+++ client_jetson.py
@@ -11,20 +11,17 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Quiet tensorflow error messages
 
 class Client:
     def __init__(self, ip_address, max_round, time_delay = 5, num_samples=600, client_id = 0, experiment = 1):
-        '''
-        Urls
-        '''
+        # URLs
         self.session = requests.Session()
         self.base_url = ip_address
-        self.put_weight_url =  self.base_url + "/put_weight/" + str(client_id)
-        self.get_weight_url =  self.base_url + "/get_server_weight" # Url that we send or fetch weight parameters
+        self.update_num_data_url = self.base_url + "/update_num_data/" + str(client_id)
+        self.put_weight_url =  self.base_url + "/put_local_weight/" + str(client_id)
+        self.get_weight_url =  self.base_url + "/get_server_weight"
         self.round_url =  self.base_url + "/get_server_round" 
         self.get_model_url = self.base_url + "/get_server_model"
         self.get_compile_config_url = self.base_url + "/get_compile_config"
         
-        '''
-        Initial setup
-        '''
+        # setting up variables
         self.experiment = experiment
         self.client_id = client_id
         self.time_delay = time_delay
@@ -32,30 +29,20 @@ class Client:
         self.current_round = 0
         self.max_round = max_round # Set the maximum number of rounds
         
-        '''
-        Downloads MNIST dataset and prepares (train_x, train_y), (test_x, test_y)
-        '''
+        # Download mnist dataset
         self.train_images, self.train_labels, self.test_images, self.test_labels = self.prepare_images()
         
         
-        
+        # Splits dataset (iid, random, )
         self.split_train_images, self.split_train_labels = self.data_split(num_samples)
         self.local_data_num = len(self.split_train_labels)
         
-        '''
-        Builds model from server
-        '''
-        
+        # Brings model architecture, optimizer, loss, metrics from the server  
         self.model = self.build_model_from_server()
         
         
         
     def prepare_images(self):
-        """
-        return: 
-            None : Prepares MNIST images in the required format for each model
-            
-        """
         mnist = tf.keras.datasets.mnist
         (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
         train_images, test_images = train_images / 255, test_images / 255
@@ -67,29 +54,22 @@ class Client:
     
     def build_model_from_server(self):
         model = self.session.get(self.get_model_url).json()
-        model = tf.keras.models.model_from_json(model, custom_objects={"null":None}) # None converted in null, which is unexpected..
-                                                                                     # Remap null to None
+
+        # None converted in null, which is unexpected..
+        # Remap null to None
+        model = tf.keras.models.model_from_json(model, custom_objects={"null":None}) 
+        
         optimizer, loss, metrics = self.request_compile_config()
         model.compile(optimizer=optimizer,loss=loss,metrics=metrics)
         return model
         
         
     def data_split(self, num_samples):
-        """
-        @params: 
-            num_samples : The number of sample images in each client. This value is used for equally
-                          sized dataset
-        
-        @return: 
-            None : Split the dataset depending on the self.experiment value
-           
-                If self.experiment is 1: Uniform data split: We take equal amount of data from each class (iid)
-                If self.experiment is 2: Random data split1: We take equal amount of data, but not uniformly distributed across classes
-                If self.experiment is 3: Random data split2: We take different amount of data and not uniformly distributed across classes
-                If self.experiment is 4: Skewed: We take disproportionate amount of data for some classes
-                        
-        """
-        
+        # Take a batch of num_samples according to self.experiment
+        # 1 : iid 
+        # 2 : Randomly selected, equally sized dataset
+        # 3 : Randomly selected, differently sized dataset
+        # 4 : Skewed dataset
         train_index_list = [[], [], [], [], [], [], [], [], [], []]
         test_index_list = [[], [], [], [], [], [], [], [], [], []]
 
@@ -132,9 +112,9 @@ class Client:
      
   
         elif self.experiment == 4: #Skewed
-            temp = [i for i in range(10)]
-            skewed_numbers = np.random.choice(temp, np.random.randint(1, 10))
-            non_skewed_numbers = list(set(temp)-set(skewed_numbers))
+            all_labels = [i for i in range(10)]
+            skewed_numbers = np.random.choice(all_labels, np.random.randint(1, 10))
+            non_skewed_numbers = set(all_labels)-set(skewed_numbers)
             N = 0
           
             for i in skewed_numbers:
@@ -161,6 +141,7 @@ class Client:
             
             self.local_data_num = N
         
+        # convert split dataset into numpy array
         split_train_images = np.array(split_train_images)
         split_train_labels = np.array(split_train_labels)
         return split_train_images, split_train_labels
@@ -168,15 +149,11 @@ class Client:
         
         
     def update_total_num_data(self, num_data):
-        """
-        num_data : the number of training images that the current client has
-        
-        update the total number of training images that is stored in the server
-        """
-        update_num_data_url =  self.base_url + "/update_num_data/"+str(self.client_id)+"/"+str(num_data)
-        self.session.get(update_num_data_url)
+        # Upload client's num_data to the server 
+        self.session.get(self.update_num_data_url +"/" + str(num_data))
         
     def request_compile_config(self):
+        # Request optimizer, loss, metrics defined in the server
         compile_config = self.session.get(self.get_compile_config_url).json()
         optim = tf.keras.optimizers.deserialize(compile_config["optim"])
         loss = tf.keras.losses.deserialize(compile_config["loss"])
@@ -184,17 +161,13 @@ class Client:
         return optim, loss, metrics
     
     def request_global_round(self):
-        """
-        result : Current global round that the server is in
-        """
+        # Request the global round
         result = self.session.get(self.round_url)
         result = result.json()
         return result
     
     def request_global_weight(self):
-        """
-        global_weight : Up-to-date version of the model parameters
-        """
+        # Request the global model (parameters)
         result = self.session.get(self.get_weight_url)
         result_data = result.json()
         
@@ -208,21 +181,15 @@ class Client:
         return global_weight
 
     def upload_local_weight(self, local_weight):
-        """
-        local_weight : the local weight that current client has converged to
-        
-        Add current client's weights to the server (Server accumulates these from multiple clients and computes the global weight)
-        """
+        # Upload trained local model parameters
         for i in range(len(local_weight)):
             local_weight[i] = local_weight[i].tolist()
         local_weight_to_json = json.dumps(local_weight)
         self.session.put(self.put_weight_url, data=local_weight_to_json)
         
     def train_local_model(self):
+        # Train a local model from latest model parameters 
         print("train started")
-        """
-        local_weight : local weight of the current client after training
-        """
         global_weight = self.request_global_weight()
         if global_weight != None:
             global_weight = list(map(lambda weight: np.array(weight), global_weight))
@@ -240,17 +207,14 @@ class Client:
         2. If the global round = current client's round, the client needs update
         3. Otherwise, we need to wait until other clients to finish
         """
-        
-        #this is for executing on multiple devices
+
         self.global_round = self.request_global_round()
-        
-        print("global round", self.global_round)
-        print(f"Client {self.client_id} current round {self.current_round}")
-        if self.current_round >= self.max_round:
+        print(f"Client {self.client_id} current round {self.current_round} global round {self.global_round}")
+        if self.current_round >= self.max_round: #need to terminate
             print(f"Client {self.client_id} finished")
             return 
 
-        if self.global_round == self.current_round: #need update 
+        if self.global_round == self.current_round: #need to update 
             print(f"Client {str(self.client_id)} needs update")
             self.split_train_images, self.split_train_labels = self.data_split(num_samples=self.local_data_num)
             self.update_total_num_data(self.local_data_num) 
