18ae2d0eb60015fb449394e8d77f3ded2d0acc2d
crazyboy9103 <gil9103@gmail.com>
2022-03-02 14:24:31 +0900

    put_local_weight error

--- FL_Server/FL_Server/urls.py
+++ FL_Server/FL_Server/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('get_compile_config', views.get_compile_config),
     path('get_server_model', views.get_server_model),
     path('get_server_weight', views.get_server_weight),
-    path('put_local_weight/<int:client_id>', views.put_weight),
+    path('put_local_weight/<int:client_id>', views.put_local_weight),
     path("update_num_data/<int:client_id>/<int:num_data>", views.update_num_data),
     path('reset/', views.reset),
  ]
