a202849ff197ac0159779842cb5e1c0ae508eac2
crazyboy9103 <gil9103@gmail.com>
2022-03-14 14:21:42 +0900

    clean

--- socket/test_client.py
+++ socket/test_client.py
@@ -1,9 +1,10 @@
 
-#이름 변경 예정
-from json_socket import Client, Message
+from pickle_socket import Client, Message
 client = Client()
 client.connect(id=0, host='127.0.0.1', port=20000) 
+#client.send(any python object)
 client.send({"test":"test"})
-response = client.recv() # blocking 
-print("client received", response)
+response = client.recv() # blocking
 #response도 Message 인스턴스
+print("client received", response)
+
